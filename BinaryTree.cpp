009fb35b3cb9e9872d83f5092f58aac3936fd4b0
shreyashingle2004@gmail.com <shreyashingle2004@gmail.com>
2023-10-29 15:36:14 +0530

    Tree commit

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -16,6 +16,7 @@ public:
     }
 };
 
+//Using Recursion
 Node *createNode(Node *root)
 {
     int data;
@@ -70,11 +71,95 @@ void LevelOrderTraversal(Node *root)
     }
 }
 
+void Inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    Inorder(root->left);
+    cout << root->data << " ";
+    Inorder(root->right);
+}
+void Preorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    Preorder(root->left);
+    Preorder(root->right);
+}
+void Postorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    Postorder(root->left);
+    Postorder(root->right);
+    cout << root->data << " ";
+}
+//using Nonrecursion
+// Node bulidFromLevelOrderTraveral(Node* &root)
+// {
+//     queue<Node *> q;
+//     cout << "Enter data for root: ";
+//     int data;
+//     cin >> data;
+
+//     root = new Node(data);
+//     q.push(root);
+
+//     while (!q.empty())
+//     {
+//         Node *temp = q.front();
+//         q.pop();
+
+//         cout << "Enter Left data for: " << temp->data << endl;
+//         int leftData;
+//         cin >> leftData;
+
+//         if (leftData != -1)
+//         {
+//             temp->left = new Node(leftData);
+//             q.push(temp->left);
+//         }
+//         cout << "Enter Right data for: " << temp->data << endl;
+//         int rightData;
+//         cin >> rightData;
+
+//         if (rightData != -1)
+//         {
+//             temp->right = new Node(rightData);
+//             q.push(temp->right);
+//         }
+//     }
+// }
+
 int main(int argc, char const *argv[])
 {
     Node *root = NULL;
     // 1 3 7 -1 -1  11 -1 -1 5 17 -1 -1 -1
+
+    // bulidFromLevelOrderTraveral(root);
+    // LevelOrderTraversal(root);
     root = createNode(root);
     LevelOrderTraversal(root);
+
+    cout << "Inorder: ";
+    Inorder(root);
+    cout << endl;
+    cout << "Preorder: ";
+    Preorder(root);
+    cout << endl;
+    cout << "Postorder: ";
+    Postorder(root);
+    cout << endl;
+
     return 0;
 }
